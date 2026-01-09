5e18309f193aa15e941857c7906ab198bde21618
JosephTseng <zxp930110@hotmail.com.tw>
2026-01-09 11:14:02 +0800

    improve drawer style

--- components/ExpenseContainer.tsx
+++ components/ExpenseContainer.tsx
@@ -9,10 +9,9 @@ const ExpenseContainer: React.FC<Props> = ({ children }) => {
     return (
         <div
             className={classNames(
-                "bg-surface min-h-[70vh] shadow-2xl flex flex-col overflow-auto animate-in slide-in-from-bottom-8 fade-in duration-200",
-                "w-full sm:w-160",
+                "bg-surface min-h-[70dvh] shadow-2xl flex flex-col overflow-auto animate-in slide-in-from-bottom-8 fade-in duration-200",
+                "max-h-[85dvh] w-full sm:w-160",
                 "fixed bottom-0 sm:top-1/2 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:-translate-y-1/2",
-                "max-h-[90vh] sm:max-h-[85vh]",
                 "rounded-t-3xl sm:rounded-2xl"
             )}
         >
