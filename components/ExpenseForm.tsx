6f24a93e04f6fde2d1f50ab77f0ef5fdb8ff58bd
JosephT5566 <30860126+JosephT5566@users.noreply.github.com>
2026-01-27 07:34:02 +0800

    Update ExpenseForm.tsx
    Import useMemo

--- components/ExpenseForm.tsx
+++ components/ExpenseForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import classNames from "classnames";
 import { isEmpty } from "lodash";
 import { AddExpenseRequest } from "../src/types";
