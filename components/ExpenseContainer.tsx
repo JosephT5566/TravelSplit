e605c1bd1062df0b5daf22a334b1f9bb5828b5f4
JosephTseng <zxp930110@hotmail.com.tw>
2026-05-05 18:31:06 +0800

    fix style

--- components/ExpenseContainer.tsx
+++ components/ExpenseContainer.tsx
@@ -9,7 +9,7 @@ const ExpenseContainer: React.FC<Props> = ({ children }) => {
     return (
         <div
             className={classNames(
-                "bg-surface min-h-[70dvh] shadow-2xl flex flex-col overflow-auto animate-in slide-in-from-bottom-8 fade-in duration-200",
+                "bg-surface shadow-2xl flex flex-col overflow-auto animate-in slide-in-from-bottom-8 fade-in duration-200",
                 "max-h-[85dvh] w-full sm:w-160",
                 "fixed bottom-0 sm:top-1/2 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:-translate-y-1/2",
                 "rounded-t-3xl sm:rounded-2xl"
